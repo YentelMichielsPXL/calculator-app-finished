140d05122e376737134663d4f6abe3cedfb77063
Yentel <148763097+YentelMichielsPXL@users.noreply.github.com>
2024-11-26 11:49:37 +0100

    Update app.js
    test for using ipv4 instead of ipv6

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const routes = require('./routes');
 const path = require('path')
 
 const app = express();
-
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on port 3000');
+});
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/calc', routes);
 
